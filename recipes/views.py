7f3e941e7f810c85c7ce297879903b9e5679d466
ZolsKe <zoltan.the.kepes@gmail.com>
2022-09-17 19:11:35 +0100

    Adding the Search Feature to our API

--- recipes/views.py
+++ recipes/views.py
@@ -1,74 +1,46 @@
-
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_project.permissions import IsOwnerOrReadOnly
 from .models import Recipe
 from .serializers import RecipeSerializer
-from django.http import Http404
-from django_project.permissions import IsOwnerOrReadOnly
 
 
-class RecipeList(APIView):
+class RecipeList(generics.ListCreateAPIView):
+    """
+    List posts or create a post if logged in
+    The perform_create method associates the post with the logged in user.
+    """
     serializer_class = RecipeSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Recipe.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes__created_at',
     ]
 
-    def get(self, request):
-        recipes = Recipe.objects.all()
-        serializer = RecipeSerializer(
-            recipes, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-    def post(self, request):
-        serializer = RecipeSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-        
-        
-class RecipeDetail(APIView):
-    permission_classes = [IsOwnerOrReadOnly]
+
+class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
+    """
     serializer_class = RecipeSerializer
-
-    def get_object(self, pk):
-        try:
-            recipe = Recipe.objects.get(pk=pk)
-            self.check_object_permissions(self.request, recipe)
-            return recipe
-        except Recipe.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        recipe = self.get_object(pk)
-        serializer = RecipeSerializer(
-            recipe, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        recipe = self.get_object(pk)
-        serializer = RecipeSerializer(
-            recipe, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        recipe = self.get_object(pk)
-        recipe.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Recipe.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
