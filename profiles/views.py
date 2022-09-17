9f5ffb59c74b009ee86651835aa06dcccbb66437
ZolsKe <zoltan.the.kepes@gmail.com>
2022-09-17 19:25:14 +0100

    Adding the filter feature to our API

--- profiles/views.py
+++ profiles/views.py
@@ -1,49 +1,6 @@
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from .models import Profile
-# from .serializers import ProfileSerializer
-# from django.http import Http404
-# from rest_framework import status
-# from django_project.permissions import IsOwnerOrReadOnly
-
-
-# class ProfileList(APIView):
-#     """
-#     List all profiles
-#     No Create view (post method), as profile creation handled by django signals
-#     """
-#     def get(self, request):
-#         profiles = Profile.objects.all()
-#         serializer = ProfileSerializer(profiles, many=True, context={'request': request})
-#         return Response(serializer.data)
-    
-
-# class ProfileDetail(APIView):
-#     serializer_class = ProfileSerializer
-#     permission_classes = [IsOwnerOrReadOnly]
-#     def get_object(self, pk):
-#         try:
-#             profile = Profile.objects.get(pk=pk)
-#             self.check_object_permissions(self.request, profile)
-#             return profile
-#         except Profile.DoesNotExist:
-#             raise Http404
-    
-#     def get(self, request, pk):
-#         profile = self.get_object(pk)
-#         serializer = ProfileSerializer(profile, context={'request': request})
-#         return Response(serializer.data)
-    
-#     def put(self, request, pk):
-#         profile = self.get_object(pk)
-#         serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from django_project.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -61,7 +18,11 @@ class ProfileList(generics.ListAPIView):
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
     ]
     ordering_fields = [
         'recipes_count',
