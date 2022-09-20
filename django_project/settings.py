76663a05266ef3e482e0a16c775bd83b0305ceee
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2022-09-20 17:28:29 +0000

    add import re

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 # if the file env.py exist (only in local production), than import env
 if os.path.exists('env.py'):
@@ -176,7 +177,8 @@ DATABASES = {
 
 # UPDATED version
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
